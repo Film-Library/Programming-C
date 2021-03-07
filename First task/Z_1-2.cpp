5f72937ecb9b7ac0cd4857cd95e9a24317363b09
Kristina <krissty87@mail.ru>
2021-03-07 17:14:46 +0300

    Задание 1-2
    Создать консольное приложение для решения задачи, представленной
    в таблице. Данные для решения вводит пользователь. Помните, что ввод
    необходимо проверять на правильность (только числа).
    Вывести результат вычислений на экран.
    При необходимости дополнить свой отчёт поясняющими формулами,
    помогающими решить задачу. Дополнить свой отчёт блок-схемой алгоритма.

---
+++ First task/Z_1-2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a;
+    double b;
+    cout << "Введите два числа";
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+
+    cout << (a * a * a + b * b * b)/2 << "  " << sqrt(fabs(a * b)) << "\n";
+
+}
+
