bda90d6aab1cfff732de17e0e439d13fd9f83f85
Kristina <krissty87@mail.ru>
2021-03-17 15:55:00 +0300

    Задания

---
+++ Zadanie 2-2/Zadanie 2-2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+double f1(const double x, const double a);
+double f2(const double x, const double a);
+
+    int main() { 
+      
+    setlocale(LC_ALL, "RUSSIAN");
+
+    const auto a = 0.3;
+    double x;
+    cout << "Введите переменную х = ";
+    cin >> x;
+
+ 
+
+    if (x * a < 1)
+    {
+        cout << "y =  " << f1(x, a);
+    }
+    else (x * a >= 1);
+    {
+        cout << "y =  " << f2(x, a);
+    }
+   
+    double f1(const double x, const double a);
+    {
+        return a * sin((x * x - 1) / 10);
+    }
+    double f2(const double x, const double a);
+    {
+        return a * cos((x - 1) / 10);
+    }
+    return(0);
+}
---
+++ Zadanie 3-1/Zadanie 3-1.cpp
@@ -0,0 +1,17 @@
+﻿
+int main()
+{
+	const auto leftBound = 1.0;
+	const auto rigtBound = 0.5;
+	const auto step = 0.1;
+
+	auto x = leftBound;
+
+	while (x <= leftBound)
+	{
+		//cout << " x = " << x << "y = " << y:
+		x += step;
+		x = x = step;
+	}
+
+}////20:50 - время лекции досмотреть
