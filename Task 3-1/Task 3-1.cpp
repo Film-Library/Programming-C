5ff2fa66d55cdfc11cf6649bbba452cd31704ada
Kristina <krissty87@mail.ru>
2021-03-17 18:07:19 +0300

    Внесены правки в код

--- Task 3-1/Task 3-1.cpp
+++ Task 3-1/Task 3-1.cpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-
-
+double y(const double x);
 
 int main()
 {
+
 	setlocale(LC_ALL, "RUSSIAN");
+
 	const auto leftBound = 1.0;
 	const auto rigtBound = 0.5;
 	const auto step = 0.1;
@@ -19,9 +20,13 @@ int main()
 
 	while (x <= leftBound)
 	{
-		//cout << " x = " << x << "y = " << y:
+		cout << " x = " << x << "y = " << y(x) << endl;
 		x += step;
 		x = x = step;
 	}
 
-}////20:50 - время лекции досмотреть
+	double y(const double x);
+	{
+		return x + cos(pow(x, 0.52) + 2);
+	}
+}
