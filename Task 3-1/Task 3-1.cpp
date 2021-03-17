3b3d005136e0095fcca1866110c6c413214f8492
Kristina <krissty87@mail.ru>
2021-03-17 17:43:55 +0300

    Задание 3-1
    Протабулировать заданную в таблице функцию. Использовать данные
    в таблице значения шага и интервала в качестве ввода пользователя
    для решения тестового примера. При невозможности расчёта функции
    в конкретной точке выводить её значение и надпись, означающую
    отсутствие решения.
    ВАРИАНТ 11

---
+++ Task 3-1/Task 3-1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	const auto leftBound = 1.0;
+	const auto rigtBound = 0.5;
+	const auto step = 0.1;
+
+	auto x = leftBound;
+
+	while (x <= leftBound)
+	{
+		//cout << " x = " << x << "y = " << y:
+		x += step;
+		x = x = step;
+	}
+
+}////20:50 - время лекции досмотреть
